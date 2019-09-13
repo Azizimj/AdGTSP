4424645ba474f7aa46989d336efeac719a89317f
MJazizi <azizim@usc.edu>
2019-09-13 12:09:44 -0700

    res review

--- res/hpc/res_review.py
+++ res/hpc/res_review.py
@@ -13,11 +13,11 @@
 import pandas, numpy as np, re, matplotlib.pyplot as plt
 
 
-def extract_plot(name):
-    print(name)
+def extract_plot(name, sig, ave):
+    print(name+" "+str(sig)+" "+str(ave))
     chosen = np.zeros((1, dim))
     for i, row in enumerate(rd[name]):
-        if str(row) != 'nan':
+        if str(row) != 'nan' and str(rd['Algo'][i]) == 'AdGTSP ':
             # print(row)
             tmp = re.findall('\.\d+',row)
             tmp = list(map(float, tmp))
@@ -26,11 +26,14 @@ def extract_plot(name):
             chosen = np.vstack((tmp,chosen))
 
     chosen = chosen[1:,:]
+    number_ch = chosen.shape[0]
 
-    plt.plot(chosen[:,0],chosen[:,1],'o', color='black')
-    plt.title(name)
+    plt.plot(chosen[:,0],chosen[:,1],'o', color='black', markersize=0.5)
+    plt.title(name+" sig: "+str(sig)+" ave: "+str(ave)+" "+str(number_ch)+' points')
+    plt.xlim([0, limits_[0]])
+    plt.ylim([0, limits_[1]])
     # plt.show()
-    plt.savefig(name+'.png')
+    plt.savefig(name+"_"+str(sig)+"_"+str(ave)+'.png')
 
     print(chosen, chosen.shape)
     print('\n\n')
@@ -38,9 +41,12 @@ def extract_plot(name):
 
 if __name__ == '__main__':
 
-    rd = pandas.read_csv('res.csv')
+    sig = 0.1
+    ave = 0.5
+    rd = pandas.read_csv('res_'+str(sig)+"_"+str(ave)+'.csv')
     num_ex = len(rd['x'])
     dim = int(rd['dim'][1])
+    limits_ = list(map(float, re.findall('\d+',(rd['limits_'][1]))))
 
-    extract_plot('chosen')
-    extract_plot('data_points')
+    extract_plot('chosen', sig, ave)
+    extract_plot('data_points', sig, ave)
